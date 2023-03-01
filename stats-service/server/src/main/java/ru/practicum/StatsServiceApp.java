f78289d3c88acb3a728d263009115fd1ab3ef9b9
Daniil Solopov <daniilsolopovdevj@gmail.com>
2023-03-01 19:05:33 +0100

    and one more

--- stats-service/server/src/main/java/ru/practicum/StatsServiceApp.java
+++ stats-service/server/src/main/java/ru/practicum/StatsServiceApp.java
@@ -2,7 +2,7 @@ package ru.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//for new commit
+
 @SpringBootApplication
 public class StatsServiceApp {
     public static void main(String[] args) {
