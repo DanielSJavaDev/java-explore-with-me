55ac9fc2e92de1cb1e0cc4362b5fdb5c78a41d86
Daniil Solopov <daniilsolopovdevj@gmail.com>
2023-03-04 13:34:33 +0100

    one more try

--- ewm-service/src/main/java/ru/practicum/ewm/config/HitClientConfig.java
+++ ewm-service/src/main/java/ru/practicum/ewm/config/HitClientConfig.java
@@ -14,6 +14,7 @@ public class HitClientConfig {
     @Bean
     public HitClient hitClient() {
         RestTemplateBuilder builder = new RestTemplateBuilder();
+        //comment
         return new HitClient(url, builder);
     }
 }
